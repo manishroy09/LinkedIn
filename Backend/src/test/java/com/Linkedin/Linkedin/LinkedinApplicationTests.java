e6519522fec8f56d0d34a7d6729e8f3e8c6558a1
manishroy09 <manishroy2163@gmail.com>
2025-09-26 14:00:16 +0530

    create maven project with java for backend

---
+++ Backend/src/test/java/com/Linkedin/Linkedin/LinkedinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Linkedin.Linkedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
